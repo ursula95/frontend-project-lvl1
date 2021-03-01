54ca70cd592733f16c9a9dfc57698ac2ff62412f
Ursula95 <tatyana2101.kuznetsova@yandex.ru>
2021-03-01 13:20:12 +0700

    change calc

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -7,13 +7,13 @@ const userName = sayHelloByName();
 console.log('What is the result of the expression?');
 
 let counter = 0;
-const operators = [ '+', '-', '*' ];
+const operators = ['+', '-', '*'];
 while (counter < 3) {
   const currentNumber = Math.ceil(Math.random() * 100);
   const currentNumberSecond = Math.ceil(Math.random() * 100);
   const currentOperator = operators[Math.floor(Math.random() * operators.length)];
   const correctAnswer = eval(`${currentNumber}${currentOperator}${currentNumberSecond}`);
-  
+
   console.log(`Question: ${currentNumber} ${currentOperator} ${currentNumberSecond}`);
   const answerOfUser = readlineSync.question('Your answer: ');
 
@@ -21,10 +21,10 @@ while (counter < 3) {
     console.log('Correct!');
     counter += 1;
   } else {
-      console.log(`'${answerOfUser}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}`);
-      break;
-    }
+    console.log(`'${answerOfUser}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}`);
+    break;
   }
+}
 
 if (counter === 3) {
   console.log(`Congratulations, ${userName}!`);
